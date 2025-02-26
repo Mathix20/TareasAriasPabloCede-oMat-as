5962865f050f04e7f87e072fab69a46fa26177d7
Mathix20 <57227200+Mathix20@users.noreply.github.com>
2025-02-25 21:41:28 -0600

    Add files via upload
    Se arreglan los errores de flake8 para el archivo de errores

---
+++ tarea_1_erroresCorregidos.py
@@ -0,0 +1,8 @@
+def saludo():  # Se agrega un espacio
+    print("Hello, World!")
+
+
+saludo()
+print("Hello, World!")
+# Línea de código demasiado larga (más de 79 caracteres)
+print("Esta línea es más corta")
